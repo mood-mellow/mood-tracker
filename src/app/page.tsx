07f8fdb94c761dbfb31c6e1219046bd0a36dc6dd
James Yab <james.luu.yab@gmail.com>
2025-07-31 17:56:10 -0400

    add initial button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Hero45 } from "~/components/hero45";
+import { Button } from "~/components/ui/button";
 
 export default function HomePage() {
   return (
     <main className="flex items-center justify-center">
+      <Button>Test</Button>
       <Hero45 heading="The Mood Tracker built by REDACTED" />
     </main>
   );
