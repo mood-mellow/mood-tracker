dab59fc48fa9375928422c1fca6f383221cacf76
James Yab <james.luu.yab@gmail.com>
2025-08-01 12:56:22 -0400

    eslint and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { Hero45 } from "~/components/hero45";
-import { Button } from "~/components/ui/button";
-import { Spinner } from "~/components/ui/spinner";
 
 export default function HomePage() {
   return (
     <main className="flex items-center justify-center">
-      <Button loading={true}>Sign-in now</Button>
       <Hero45 heading="The Mood Tracker built by REDACTED" />
     </main>
   );
--- src/components/hero45.tsx
+++ src/components/hero45.tsx
@@ -1,4 +1,5 @@
 import { HandHelping, Users, Zap } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 import { Badge } from "~/components/ui/badge";
@@ -52,12 +53,14 @@ const Hero45 = ({
           <h1 className="text-4xl font-semibold lg:text-5xl">{heading}</h1>
         </div>
         <div className="relative mx-auto max-w-5xl">
-          <img
+          <Image
+            height={0}
+            width={0}
             src={imageSrc}
             alt={imageAlt}
             className="aspect-video max-h-[500px] w-full rounded-xl object-cover"
           />
-          <div className="absolute inset-0 bg-linear-to-t from-background via-transparent to-transparent"></div>
+          <div className="from-background absolute inset-0 bg-linear-to-t via-transparent to-transparent"></div>
           <div className="absolute -top-28 -right-28 -z-10 aspect-video h-72 w-96 [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_20%,transparent_100%)] [background-size:12px_12px] opacity-40 sm:bg-[radial-gradient(hsl(var(--muted-foreground))_1px,transparent_1px)]"></div>
           <div className="absolute -top-28 -left-28 -z-10 aspect-video h-72 w-96 [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_20%,transparent_100%)] [background-size:12px_12px] opacity-40 sm:bg-[radial-gradient(hsl(var(--muted-foreground))_1px,transparent_1px)]"></div>
         </div>
@@ -67,18 +70,18 @@ const Hero45 = ({
               {index > 0 && (
                 <Separator
                   orientation="vertical"
-                  className="mx-6 hidden h-auto w-[2px] bg-linear-to-b from-muted via-transparent to-muted md:block"
+                  className="from-muted to-muted mx-6 hidden h-auto w-[2px] bg-linear-to-b via-transparent md:block"
                 />
               )}
               <div
                 key={index}
-                className="flex grow basis-0 flex-col rounded-md bg-background p-4"
+                className="bg-background flex grow basis-0 flex-col rounded-md p-4"
               >
-                <div className="mb-6 flex size-10 items-center justify-center rounded-full bg-background drop-shadow-lg">
+                <div className="bg-background mb-6 flex size-10 items-center justify-center rounded-full drop-shadow-lg">
                   {feature.icon}
                 </div>
                 <h3 className="mb-2 font-semibold">{feature.title}</h3>
-                <p className="text-sm text-muted-foreground">
+                <p className="text-muted-foreground text-sm">
                   {feature.description}
                 </p>
               </div>
