b599c0aa94c2bdaa1c00f4463e343c4fd184e71b
James Yab <james.luu.yab@gmail.com>
2025-08-05 19:20:19 -0400

    fix lint warnings

--- src/components/registerForm.tsx
+++ src/components/registerForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { z } from "zod";
+import { type z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
