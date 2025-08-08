1d40aae79f52ecb158f99cc3e8d01ddc41b2f87b
James Yab <james.luu.yab@gmail.com>
2025-08-08 15:10:05 -0400

    move amplify configuration to a separate file

--- src/components/authProvider.tsx
+++ src/components/authProvider.tsx
@@ -1,38 +1,10 @@
 "use client";
 
 import { Authenticator } from "@aws-amplify/ui-react";
-import { Amplify, type ResourcesConfig } from "aws-amplify";
+import { Amplify } from "aws-amplify";
+import { amplifyConfig } from "~/lib/amplifyConfig";
 
-const config: ResourcesConfig = {
-  Auth: {
-    Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID!,
-      userPoolClientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_CLIENT_ID!,
-      identityPoolId: process.env.NEXT_PUBLIC_AWS_IDENTITY_POOL_ID!,
-      loginWith: {
-        email: true,
-      },
-      signUpVerificationMethod: "code",
-      userAttributes: {
-        email: {
-          required: true,
-        },
-        preferred_username: {
-          required: true,
-        },
-      },
-      allowGuestAccess: false,
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
-      },
-    },
-  },
-};
-Amplify.configure(config, { ssr: true });
+Amplify.configure(amplifyConfig, { ssr: true });
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   return <Authenticator.Provider>{children}</Authenticator.Provider>;
--- src/lib/amplifyServerUtils.ts
+++ src/lib/amplifyServerUtils.ts
@@ -1,36 +1,6 @@
 import { createServerRunner } from "@aws-amplify/adapter-nextjs";
-import type { ResourcesConfig } from "aws-amplify";
-
-const config: ResourcesConfig = {
-  Auth: {
-    Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID!,
-      userPoolClientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_CLIENT_ID!,
-      identityPoolId: process.env.NEXT_PUBLIC_AWS_IDENTITY_POOL_ID!,
-      loginWith: {
-        email: true,
-      },
-      signUpVerificationMethod: "code",
-      userAttributes: {
-        email: {
-          required: true,
-        },
-        preferred_username: {
-          required: true,
-        },
-      },
-      allowGuestAccess: false,
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
-      },
-    },
-  },
-};
+import { amplifyConfig } from "~/lib/amplifyConfig";
 
 export const { runWithAmplifyServerContext } = createServerRunner({
-  config,
+  config: amplifyConfig,
 });
