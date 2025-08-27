343237dd96e3e45c9ab8d87141d875fee8f9eee4
James Yab <james.luu.yab@gmail.com>
2025-08-27 13:43:17 -0400

    modify the button to allow for editing the content style and better styling for icons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -27,7 +27,7 @@ const buttonVariantConfig = {
 };
 
 const buttonVariants = cva(
-  "grid items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "grid cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: buttonVariantConfig,
     defaultVariants: {
@@ -44,11 +44,13 @@ function Button({
   asChild = false,
   loading = false,
   children,
+  contentStyles,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
     loading?: boolean;
+    contentStyles?: string;
   }) {
   const Comp = asChild ? Slot : "button";
 
@@ -63,7 +65,13 @@ function Button({
       {...props}
     >
       {/* Text */}
-      <span className={cn("transition-opacity", loading && "opacity-0")}>
+      <span
+        className={cn(
+          "flex items-center gap-2 transition-opacity",
+          contentStyles,
+          loading && "opacity-0",
+        )}
+      >
         {children}
       </span>
 
