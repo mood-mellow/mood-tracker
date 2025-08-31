5e700d8fe05010ce85e8a73938c8d4a714602471
James Yab <james.luu.yab@gmail.com>
2025-08-31 00:23:00 -0400

    replace id mapping from Long to String

--- backend/src/main/java/com/moodtracker/backend/repository/MoodEntryRepository.java
+++ backend/src/main/java/com/moodtracker/backend/repository/MoodEntryRepository.java
@@ -1,7 +1,6 @@
 package com.moodtracker.backend.repository;
 
 import com.moodtracker.backend.model.MoodEntry;
-import com.moodtracker.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface MoodEntryRepository extends JpaRepository<MoodEntry, Long> {
+public interface MoodEntryRepository extends JpaRepository<MoodEntry, String> {
     List<MoodEntry> findByUserIdAndTimestampBetween(
             String userId,
             LocalDateTime start,
