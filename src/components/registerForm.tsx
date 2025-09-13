4a22130a500efe52645ad111944b54b247be4090
James Yab <james.luu.yab@gmail.com>
2025-09-13 11:27:41 -0400

    update responsive styling on the form

--- src/components/registerForm.tsx
+++ src/components/registerForm.tsx
@@ -112,7 +112,7 @@ export default function RegisterForm() {
           />
         </div>
 
-        <div className="flex flex-1 flex-col justify-center gap-2">
+        <div className="flex flex-1 flex-col justify-center gap-2 md:mt-8">
           <CardHeader className="px-0">
             <CardTitle className="text-center text-2xl md:text-left">
               Create an Account
@@ -218,22 +218,22 @@ export default function RegisterForm() {
                     )}
                   />
                 </div>
-                <div className="mt-4 flex items-center justify-end gap-2 text-center text-sm">
-                  <span>Already have an account? </span>
-                  <Link href="#" className="underline">
-                    Login
-                  </Link>
-                  <Button
-                    type="submit"
-                    disabled={signUpMutation.isPending}
-                    loading={signUpMutation.isPending}
-                  >
-                    Sign up
-                  </Button>
-                </div>
               </form>
             </Form>
           </CardContent>
+          <div className="mt-4 flex items-center justify-end gap-2 text-center text-sm md:mt-auto">
+            <span>Already have an account? </span>
+            <Link href="#" className="underline">
+              Login
+            </Link>
+            <Button
+              type="submit"
+              disabled={signUpMutation.isPending}
+              loading={signUpMutation.isPending}
+            >
+              Sign up
+            </Button>
+          </div>
         </div>
       </Card>
     </div>
