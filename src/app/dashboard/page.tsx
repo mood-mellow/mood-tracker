92ffd4dcae7c0670811909700327e9dc13a0fb6c
James Yab <james.luu.yab@gmail.com>
2025-09-16 19:10:53 -0400

    add responsive css grid layout for the dashboard widgets

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,12 +7,15 @@ import TestApiCall from "~/components/test/testApiCall";
 import { ActivityTagsPopover } from "~/components/activityTags/activityTagsPopover";
 import MoodEntryForm from "~/components/moodEntryForm";
 import { Navbar01 as Navbar } from "~/components/ui/shadcn-io/navbar-01";
+import "~/styles/dashboard.css";
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import React from "react";
+
 export default function DashboardPage() {
   return (
     <>
@@ -27,27 +30,37 @@ export default function DashboardPage() {
         >
           Log Out
         </Button>*/}
-        <Card>
-          <CardHeader>
-            <CardTitle>Suggestions</CardTitle>
-            <CardDescription>
-              AI generated tips based on your activity.
-            </CardDescription>
-          </CardHeader>
-        </Card>
 
-        <Card>
-          <CardHeader>
-            <CardTitle>Friends</CardTitle>
-          </CardHeader>
-        </Card>
+        <div className="dashboard-widgets-container gap-x-8 gap-y-6 md:p-12">
+          <Card className="checkin-container">
+            <CardHeader>
+              <CardTitle>Check in today!</CardTitle>
+              <CardDescription>How are you feeling?</CardDescription>
+            </CardHeader>
+          </Card>
 
-        <Card>
-          <CardHeader>
-            <CardTitle>Review</CardTitle>
-          </CardHeader>
-        </Card>
-        <MoodEntryForm />
+          <Card className="suggestions-container">
+            <CardHeader>
+              <CardTitle>Suggestions</CardTitle>
+              <CardDescription>
+                AI generated tips based on your activity.
+              </CardDescription>
+            </CardHeader>
+          </Card>
+
+          <Card className="friends-container">
+            <CardHeader>
+              <CardTitle>Friends</CardTitle>
+            </CardHeader>
+          </Card>
+
+          <Card className="review-container">
+            <CardHeader>
+              <CardTitle>Review</CardTitle>
+            </CardHeader>
+          </Card>
+          {/*<MoodEntryForm />*/}
+        </div>
       </main>
     </>
   );
