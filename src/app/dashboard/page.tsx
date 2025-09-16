82acc241d2491bc491d39e4e76679d82f752171f
James Yab <james.luu.yab@gmail.com>
2025-09-16 17:38:47 -0400

    add navbar base component for the dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,23 +6,49 @@ import { redirect } from "next/navigation";
 import TestApiCall from "~/components/test/testApiCall";
 import { ActivityTagsPopover } from "~/components/activityTags/activityTagsPopover";
 import MoodEntryForm from "~/components/moodEntryForm";
-
+import { Navbar01 as Navbar } from "~/components/ui/shadcn-io/navbar-01";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 export default function DashboardPage() {
   return (
-    <main className="flex items-center justify-center">
-      <TestApiCall />
-      <h1>Dashboard</h1>
-      <ActivityTagsPopover />
-      <Button
-        onClick={async () => {
-          console.log("test");
-          await signOut();
-          redirect("/");
-        }}
-      >
-        Log Out
-      </Button>
-      <MoodEntryForm />
-    </main>
+    <>
+      <Navbar />
+      <main className="p-8">
+        {/*<Button
+          onClick={async () => {
+            console.log("test");
+            await signOut();
+            redirect("/");
+          }}
+        >
+          Log Out
+        </Button>*/}
+        <Card>
+          <CardHeader>
+            <CardTitle>Suggestions</CardTitle>
+            <CardDescription>
+              AI generated tips based on your activity.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Friends</CardTitle>
+          </CardHeader>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Review</CardTitle>
+          </CardHeader>
+        </Card>
+        <MoodEntryForm />
+      </main>
+    </>
   );
 }
